6788b97d2a1fb725dc2e58737c16211b7b2e48e8
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2025-06-15 21:21:51 -0300

    Aula2

---
+++ Aula2_Operadores/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 10;
+int b = 20;
+
+int c = a + b;
+
+Console.WriteLine(c);
+
+c += 5;
+Console.WriteLine(c);
+
+
+//Conversão de tipos - Cast/Casting
+
+int d = Convert.ToInt32 ("5");
+Console.WriteLine(d);
+
+
+//int e = int.Parse("5c");
+//Console.WriteLine(e);
+
+
+Console.WriteLine(Convert.ToInt32(null));
+//Console.WriteLine(int.Parse(null));
+
+int inteiro = 5;
+string f = inteiro.ToString();
+Console.WriteLine(f);
