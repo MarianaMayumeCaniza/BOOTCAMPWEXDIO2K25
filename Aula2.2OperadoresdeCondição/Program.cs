0d3f4d46bb80da2b788da83bf785246137dcdd38
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2025-07-20 11:47:46 -0300

    Operadores Logicos

---
+++ Aula2.2OperadoresdeCondição/Program.cs
@@ -0,0 +1,36 @@
+﻿int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
+
+if (possivelVenda)
+{
+    Console.WriteLine("Venda realizada");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque");
+}
+
+
+
+Console.WriteLine("Digite uma letra");
+
+string letra = Console.ReadLine();
+
+switch (letra){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+    
+    default: 
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
