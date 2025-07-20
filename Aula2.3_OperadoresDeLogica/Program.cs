4ffd4b8f180ce51bff96a22cd5ae51306a5581ab
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2025-07-20 15:02:33 -0300

    operadoreslogicos

---
+++ Aula2.3_OperadoresDeLogica/Program.cs
@@ -0,0 +1,11 @@
+﻿bool ehMaiorDeIdade = true;
+bool possuiAutorizacaoDoPesponsavel = false;
+
+//Se ambos forem falsa não é liberado
+
+if( ehMaiorDeIdade || possuiAutorizacaoDoPesponsavel){
+    Console.WriteLine("Entrada liberada!");
+
+}else{
+    Console.WriteLine("Entrada não liberada");
+}
