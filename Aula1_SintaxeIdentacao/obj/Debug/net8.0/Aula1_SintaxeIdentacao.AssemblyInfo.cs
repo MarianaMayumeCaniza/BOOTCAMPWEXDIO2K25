d3f93f048a38cde51a3921287bbdc4f68d070c15
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2025-07-26 17:23:48 -0300

    Operadores Logicos

--- Aula1_SintaxeIdentacao/obj/Debug/net8.0/Aula1_SintaxeIdentacao.AssemblyInfo.cs
+++ Aula1_SintaxeIdentacao/obj/Debug/net8.0/Aula1_SintaxeIdentacao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula1_SintaxeIdentacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9766e04f982b0b3b2849c1a2de3492c45e48f427")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffd4b8f180ce51bff96a22cd5ae51306a5581ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula1_SintaxeIdentacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula1_SintaxeIdentacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula2.3_OperadoresDeLogica/Program.cs
+++ Aula2.3_OperadoresDeLogica/Program.cs
@@ -3,9 +3,39 @@ bool possuiAutorizacaoDoPesponsavel = false;
 
 //Se ambos forem falsa não é liberado
 
-if( ehMaiorDeIdade || possuiAutorizacaoDoPesponsavel){
+if (ehMaiorDeIdade || possuiAutorizacaoDoPesponsavel)
+{
     Console.WriteLine("Entrada liberada!");
 
-}else{
+}
+else
+{
     Console.WriteLine("Entrada não liberada");
+}
+
+
+
+bool possuiPresencaMinima = true;
+double media = 7.5;
+
+if (possuiPresencaMinima && media > 7)
+{
+    Console.WriteLine("Aprovado");
+
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
+
+bool choveu = true;
+bool estaTarde = true;
+
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar");
+}
+else
+{
+    Console.WriteLine("Vou andar de bicicleta outro dia");
 }
